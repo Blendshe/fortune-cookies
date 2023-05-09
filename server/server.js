66af80720abad6d9a8642eb3821801b1011856ab
warp2063 <warp2063@gmail.com>
2023-05-09 15:41:47 -0400

    add random endpoint and json data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,22 @@
+const fortunes = require("./data.json");
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
+
+app.get("/", (request, response) => {
+  response.json("hello from fortune-cookies root");
+});
+
+app.get("/random", (request, response) => {
+  const random = Math.floor(Math.random() * fortunes.length);
+
+  response.json(fortunes[random].fortune);
+});
+
+app.listen(PORT, () => {
+  console.log("App is listening on PORT " + PORT);
+});
