8f63ff11e4fa6acf935ee71ededcad1396e50eae
simpsonpaisley <simpsonpaisley@gmail.com>
2023-05-09 20:56:30 +0100

    Add frontend

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
-const fortunes = require("./data.json");
+const fortunes = require('./data.json');
 
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const PORT = process.env.PORT;
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const PORT = process.env.PORT || 2021;
 const app = express();
 app.use(cors());
 
-app.get("/", (request, response) => {
-  response.json("hello from fortune-cookies root");
+app.get('/', (request, response) => {
+	response.json('hello from fortune-cookies root');
 });
 
-app.get("/random", (request, response) => {
-  const random = Math.floor(Math.random() * fortunes.length);
+app.get('/random', (request, response) => {
+	const random = Math.floor(Math.random() * fortunes.length);
 
-  response.json(fortunes[random].fortune);
+	response.json(fortunes[random].fortune);
 });
 
 app.listen(PORT, () => {
-  console.log("App is listening on PORT " + PORT);
+	console.log('App is listening on PORT ' + PORT);
 });
